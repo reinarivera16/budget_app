e3b8183918684638e48ef4744054b68feafa0479
reinarivera16 <72185375+reinarivera16@users.noreply.github.com>
2021-04-09 16:22:27 -0500

    add files via upload

---
+++ budget-app/README.txt
@@ -0,0 +1,53 @@
+# install Node JS in System
+
+# Install Mongo DB in System
+
+#open Budget-app Folder in cmd or Terminal
+
+#run command node server.js or nmp run start
+
+#run command cd C:\Program Files\MongoDB\Server\4.4\bin> mongod OR GOTO C:\Program Files\MongoDB\Server\4.4\bin\ double click on mongod.exe
+
+#run command cd C:\Program Files\MongoDB\Server\4.4\bin> mongo OR GOTO C:\Program Files\MongoDB\Server\4.4\bin\ double click on mongo.exe
+
+#run command on terminal > npm install mongoose
+
+#run command on terminal > npm install express
+
+#Then Going With Port Provide By Server Like :Localhost:Port Example ::Localhost:3000
+
+///////////////
+
+#For Deployed On Heroku -> Don't Logout From Heroku untill deploy
+
+#Download gitbash
+
+#Create Account On Heroku
+
+#Download In this step you’ll install the Heroku Command Line Interface (CLI). Link : https://devcenter.heroku.com/articles/getting-started-with-nodejs#set-up  Download According To System
+
+# Goto Dashboard -> Create New App -> Enter App Name -> Click On "Create App"
+
+#Open Git Bash App From System
+
+#goto  "Budget-app Folder" in command
+
+#Check Git Install >with this Command "git --version"
+
+#Run Following Command - This All Command Are Available After Creating App
+
+1. git init
+
+2. git add .
+
+3. heroku git:remote -a "budget-trackers-app"
+
+4. git commit -am "make it better"
+
+5. git push heroku master
+
+# After Deploy You Can See A Link 4 Line Above Like This :: https://budget-trackers-app.herokuapp.com/ deployed to Heroku
+
+
+
+
---
+++ budget-app/public/index.js
@@ -0,0 +1,122 @@
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/service-worker.js").then((reg) => {
+      console.log("Service worker registered.", reg);
+    });
+  });
+}
+
+let transactions = [];
+let myChart;
+
+fetch("/api/transaction")
+  .then((response) => response.json())
+  .then((data) => {
+    // save db data on global variable
+    transactions = data;
+    populateTotal();
+    populateTable();
+  });
+
+function populateTotal() {
+  // reduce transaction amounts to a single total value
+  const total = transactions.reduce((total, t) => {
+    return total + parseInt(t.value);
+  }, 0);
+
+  const totalEl = document.querySelector("#total");
+  totalEl.textContent = total;
+}
+
+function populateTable() {
+  const tbody = document.querySelector("#tbody");
+  tbody.innerHTML = "";
+
+  transactions.forEach((transaction) => {
+    // create and populate a table row
+    const tr = document.createElement("tr");
+    if (transaction.value > 0) {
+      tr.classList.add("deposit");
+    } else {
+      tr.classList.add("withdraw");
+    }
+    tr.innerHTML = `
+      <td>${transaction.name}</td>
+      <td>${transaction.value}</td>
+    `;
+
+    tbody.appendChild(tr);
+  });
+}
+
+function sendTransaction(isAdding) {
+  const nameEl = document.querySelector("#t-name");
+  const amountEl = document.querySelector("#t-amount");
+  const errorEl = document.querySelector(".error");
+
+  // validate form
+  if (nameEl.value === "" || amountEl.value === "" || amountEl.value == 0) {
+    errorEl.textContent = "Missing Information";
+    return;
+  } else {
+    errorEl.textContent = "";
+  }
+
+  // create record
+  const transaction = {
+    name: nameEl.value,
+    value: amountEl.value,
+    date: new Date().toISOString(),
+  };
+
+  // if subtracting funds, convert amount to negative number
+  if (!isAdding) {
+    transaction.value *= -1;
+  }
+
+  // add to beginning of current array of data
+  transactions.unshift(transaction);
+
+  // re-run logic to populate ui with new record
+  populateTable();
+  populateTotal();
+
+  // also send to server
+  fetch("/api/transaction", {
+    method: "POST",
+    body: JSON.stringify(transaction),
+    headers: {
+      Accept: "application/json, text/plain, */*",
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.errors) {
+        errorEl.textContent = "Missing Information";
+      } else {
+        // clear form
+        nameEl.value = "";
+        amountEl.value = "";
+      }
+    })
+    .catch((err) => {
+      // fetch failed, so save in indexed db
+      console.log(transaction);
+      saveRecord(transaction);
+
+      // clear form
+      nameEl.value = "";
+      amountEl.value = "";
+    });
+}
+
+document.querySelector("#add-btn").addEventListener("click", function (event) {
+  event.preventDefault();
+  sendTransaction(true);
+});
+
+document.querySelector("#sub-btn").addEventListener("click", function (event) {
+  event.preventDefault();
+  sendTransaction(false);
+});
